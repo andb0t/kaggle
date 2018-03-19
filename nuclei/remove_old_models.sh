cb4072b95d0a81762179b6dc38ed8adefaa8578f
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-20 00:37:58 +0100

    Trash to trash dir

--- nuclei/remove_old_models.sh
+++ nuclei/remove_old_models.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-find . -regextype posix-extended -regex ".*[0-9]\.h5" -delete
+find . -regextype posix-extended -regex ".*[0-9]\.h5" -exec echo "Deleting {}" \; -exec mv {} $TRASH \;
+# find . -regextype posix-extended -regex ".*[0-9]\.h5" -delete
 
